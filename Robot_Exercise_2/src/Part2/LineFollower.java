3dbc90a119c920d3b991aa5316a70fa8bbd58150
Lemonlemmings <andrewisere@hotmail.co.uk>
2014-02-09 13:39:52 +0000

    Reverting Amars changes
    Reverting an idiotic style of programming

--- Robot_Exercise_2/src/Part2/LineFollower.java
+++ Robot_Exercise_2/src/Part2/LineFollower.java
@@ -2,8 +2,10 @@ package Part2;
 
 import lejos.nxt.Button;
 
-public class LineFollower {
-	public static void main(String[] args) {
+public class LineFollower 
+{
+	public static void main(String[] args) 
+	{
 		Button.waitForAnyPress();
 		
 		LineFollowerRun program = new LineFollowerRun();		
