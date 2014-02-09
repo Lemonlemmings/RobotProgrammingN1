bef31460d20cb02049e070f6cd60243daadca90c
Lemonlemmings <andrewisere@hotmail.co.uk>
2014-02-09 12:12:30 +0000

    Robot programming exercise 2 -pt2
    Tried testing. Added cocks and balls.

--- Robot_Exercise_2/src/Part2/LineFollowerRun.java
+++ Robot_Exercise_2/src/Part2/LineFollowerRun.java
@@ -3,8 +3,10 @@ package Part2;
 import lejos.nxt.LightSensor;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
+import lejos.nxt.SensorPortListener;
 import lejos.robotics.Color;
 import lejos.robotics.navigation.DifferentialPilot;
+import lejos.util.Delay;
 
 
 public class LineFollowerRun 
@@ -12,46 +14,65 @@ public class LineFollowerRun
 	private DifferentialPilot pilot;
 	private LightSensor left;
 	private LightSensor right;
-	private boolean quit;
+	
+	private boolean m_run = true;
+	private boolean right_steer = false;
+	private boolean left_steer = false;
+	private final int darkNum = 50; 
 	
 	LineFollowerRun() 
 	{
 		pilot = new DifferentialPilot(300, 1720, Motor.A, Motor.B, false);
 		
-		left = new LightSensor(SensorPort.S1);
+		left  = new LightSensor(SensorPort.S1);
 		right = new LightSensor(SensorPort.S2);
 		
 		left.setFloodlight(Color.WHITE);
 		right.setFloodlight(Color.WHITE);
-		
-		quit = false;
 	}
 	
 	public void run() 
 	{
-		while (!quit) 
+		SensorPort.S1.addSensorPortListener(new SensorPortListener()
 		{
-			boolean leftLight = left.getLightValue() < 25;
-			boolean rightLight = right.getLightValue() < 25;
-			
-			if (leftLight && rightLight) 
+			public void stateChanged(SensorPort aSource, int aOldValue,	int aNewValue) 
 			{
-				pilot.stop();
+				if(left.getLightValue() < darkNum)
+				{
+					System.out.println("Balls");
+					left_steer = true;
+					Delay.msDelay(2000);
+				}
 			}
 			
-			else if (leftLight) 
+		});
+		
+		SensorPort.S2.addSensorPortListener(new SensorPortListener()
+		{
+			public void stateChanged(SensorPort aSource, int aOldValue,	int aNewValue) 
 			{
-				pilot.steer(100);
+				if(right.getLightValue() < darkNum)
+				{
+					System.out.println("Cocks");
+					right_steer = true;
+					Delay.msDelay(2000);
+				}
 			}
 			
-			else if (rightLight) 
+		});
+		
+		while (m_run) 
+		{
+			pilot.forward();
+			if(right_steer)
 			{
-				pilot.steer(-100);
+				pilot.steer(30);
+				right_steer = false;
 			}
-			
-			else 
+			if(left_steer)
 			{
-				pilot.forward();
+				pilot.steer(-30);
+				left_steer = false;
 			}
 		}
 	}
