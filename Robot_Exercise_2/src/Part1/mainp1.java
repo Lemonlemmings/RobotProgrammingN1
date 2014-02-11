1d74186ba92539d750106ee1010b21e347b7c0cc
Lemonlemmings <andrewisere@hotmail.co.uk>
2014-02-11 00:02:41 +0000

    Added comments in a "meh" mood
    Added comments to part 1, no big deal really

--- Robot_Exercise_2/src/Part1/mainp1.java
+++ Robot_Exercise_2/src/Part1/mainp1.java
@@ -6,11 +6,14 @@ public class mainp1
 {
 	public static void main(String[] args)
 	{
+        //Wait for a button press before the program will begin
 		Button.waitForAnyPress();
 		
+        //Setting up the runnable object so that the main block the program can run
 		wall WallRun = new wall();
 		WallRun.run();
 		
+        //Wait for a button press to let the program exit
 		Button.waitForAnyPress();		
 	}
 }
